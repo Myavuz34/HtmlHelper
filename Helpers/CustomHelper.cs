21db42b3b4c43620ccd0644630dee117e7b2c8ab
Mustafa YAVUZ <myavuz53@gmail.com>
2018-06-19 22:41:50 +0300

    Custom Static Helper

---
+++ Helpers/CustomHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC_033_CustomHelper.Helpers
+{
+    public static class CustomHelper
+    {
+        public static MvcHtmlString Textbox(string id, string name = "")
+        {
+            if (string.IsNullOrEmpty(name))
+                name = id;
+            string inputHtml = string.Format("<input type='text' id='{0}' name='{1}'/>", id, name);
+            MvcHtmlString textbox=MvcHtmlString.Create(inputHtml);
+            return textbox;
+        }
+        public static MvcHtmlString Textbox2(this HtmlHelper htmlHelperlHelper,string id, string name ,string cssClassName)
+        {
+            if (string.IsNullOrEmpty(name))
+                name = id;
+            string inputHtml = string.Format("<input type='text' id='{0}' name='{1}' class='{2}' />", id, name,cssClassName);
+            MvcHtmlString textbox = MvcHtmlString.Create(inputHtml);
+            return textbox;
+        }
+    }
+}
